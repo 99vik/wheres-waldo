f7df2ea4b562c67238b71a740fa7a4811ee37f20
Viktor Miric <viktorm47@gmail.com>
2023-10-06 17:08:32 +0200

    Navbar with routes, index.html head modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/leaderboard">Leaderboard</Link>
+      <div className="flex justify-between items-center bg-red-700 text-white py-3 px-20">
+        <p className="text-3xl font-bold">Where&apos;s Waldo?</p>
+        <div className="flex gap-20">
+          <Link to="/">
+            <p className="text-lg ">Home</p>
+          </Link>
+          <Link to="/leaderboard">
+            <p className="text-lg">Leaderboard</p>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
