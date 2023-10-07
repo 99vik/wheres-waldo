e9f69279be2f79545708f01eedb167bca8ebe243
Viktor Miric <viktorm47@gmail.com>
2023-10-07 20:28:42 +0200

    Outputing position of picture click

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -6,10 +6,29 @@ function Game() {
   const { title } = useParams();
   const Picture = GetPicture(title);
 
+  function pictureClick(e) {
+    console.log(
+      Number((e.pageX / e.target.offsetWidth).toFixed(2)) * 100,
+      '% width'
+    );
+    console.log(
+      Number(
+        ((e.pageY - e.target.offsetTop) / e.target.offsetHeight).toFixed(2) *
+          100
+      ),
+      '% height'
+    );
+  }
+
   return (
     <>
       <Timer />
-      <img src={Picture} alt="picture" className="w-screen" />
+      <img
+        src={Picture}
+        alt="picture"
+        className="w-screen"
+        onClick={pictureClick}
+      />
     </>
   );
 }
