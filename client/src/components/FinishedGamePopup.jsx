c5c46203a3ea6e9d7bd0455b44db06e97d09abbc
Viktor Miric <viktorm47@gmail.com>
2023-10-10 23:29:27 +0200

    Getting time from server on finished game

--- client/src/components/FinishedGamePopup.jsx
+++ client/src/components/FinishedGamePopup.jsx
@@ -1,14 +1,40 @@
-function FinishedGamePopup() {
+import PropTypes from 'prop-types';
+import CloseIcon from '../assets/icons/close.svg';
+import { useState } from 'react';
+
+function FinishedGamePopup({ time }) {
+  const [popup, setPopup] = useState(true);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function closePopup() {
+    setPopup(false);
+  }
+
   return (
     <>
-      <div className="absolute top-0 left-0 w-full h-full z-20 flex items-center justify-center">
+      <div
+        className={`absolute top-0 left-0 w-full h-full z-20 flex items-center justify-center ${
+          popup ? '' : 'hidden'
+        }`}
+      >
         <div className="z-1 bg-neutral-400/40 h-full w-full absolute top-0 left-0"></div>
-        <div className="w-[50%] h-[40%] p-10 border border-black bg-white absolute z-10 translate-y-20 rounded-2xl flex flex-col justify-between items-center">
+        <div className="w-[50%] h-[45%] p-8 border border-black bg-white absolute z-10 translate-y-20 rounded-2xl flex flex-col justify-between items-center">
+          <button onClick={closePopup}>
+            <img
+              src={CloseIcon}
+              alt="close"
+              className="w-[30px] absolute top-0 right-0 hover:scale-105"
+            />
+          </button>
           <div>
             <h1 className="text-center text-3xl">
               You found all the characters!
             </h1>
-            <p className="mt-4 text-lg text-neutral-500">
+            <p className="text-center text-xl mt-2">
+              It took you {minutes} minutes and {seconds} seconds
+            </p>
+            <p className="mt-10 text-md text-neutral-500">
               Submit your time to the leaderboard and compare it with others.
             </p>
           </div>
@@ -39,5 +65,9 @@ function FinishedGamePopup() {
     </>
   );
 }
+
+FinishedGamePopup.propTypes = {
+  time: PropTypes.number,
+};
 
 export default FinishedGamePopup;
