ad42b82fa9d54b0d87a9af86bf6fa00374481712
Viktor Miric <viktorm47@gmail.com>
2023-10-11 18:03:50 +0200

    Loading data with player records in table format

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -8,13 +8,42 @@ function Leaderboard() {
     async function getData() {
       const data = await getRecords();
       setData(data);
-
-      console.log(data);
     }
     getData();
   }, []);
 
-  return <h1>Leaderboard</h1>;
+  function dataRows() {
+    const rows = data.map((record, index) => {
+      const minutes = record.minutes;
+      const seconds = record.seconds;
+
+      return (
+        <tr key={index} className="border-b border-neutral-200 bg-neutral-50">
+          <td className="px-6 py-2 font-medium whitespace-nowrap">
+            {record.name}
+          </td>
+          <td className="px-6 py-2 font-medium whitespace-nowrap">{`${
+            minutes < 10 ? `0${minutes}` : minutes
+          }:${seconds < 10 ? `0${seconds}` : seconds}`}</td>
+        </tr>
+      );
+    });
+    return rows;
+  }
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <table className="text-gray-500 text-left mt-20 w-[500px] border border-neutral-200">
+        <thead className="text-md text-white uppercase bg-neutral-600">
+          <tr>
+            <th className="px-6 py-3">Username</th>
+            <th className="px-6 py-3">Time</th>
+          </tr>
+        </thead>
+        <tbody>{data && dataRows()}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Leaderboard;
