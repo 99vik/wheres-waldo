009ba0756059c2b8b1115b4be258b596a44c5795
Viktor Miric <viktorm47@gmail.com>
2023-10-12 17:39:07 +0200

    Moved API url and rails cors origin to environment variables

--- app/models/player_record.rb
+++ app/models/player_record.rb
@@ -1,3 +1,3 @@
 class PlayerRecord < ApplicationRecord
-    validates :name, presence: true, length: { minimum: 5, maximum: 20}
+    validates :name, presence: true, length: { minimum: 5, maximum: 20}, uniqueness: true
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
+    # origins 'http://localhost:5173'
+    origins ENV['CLIENT_URL']
 
     resource "*",
       headers: :any,
